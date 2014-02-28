217c7df5f120f1c0c6636bd1182126261e98d7b9
Derek Lowes <derek3x@vmail.me>
2014-02-28 10:16:47 -0500

    fixed missing directory

--- create_keys.py
+++ create_keys.py
@@ -1,5 +1,8 @@
 #!flask/bin/python
 import keyczar
+import os
 from keyczar import keyczart
+newpath = r'tmp/kz' 
+if not os.path.exists(newpath): os.makedirs(newpath)
 keyczart.main(['create','--location=tmp/kz/','--purpose=crypt','--name=PryNotes'])
 keyczart.main(['addkey','--location=tmp/kz/' ,'--status=primary'])
