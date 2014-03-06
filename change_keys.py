48f7a419aa8ac66be96266f842dfec60ae181f84
Derek Lowes <derek3x@vmail.me>
2014-03-06 10:57:40 -0500

    adding ability to change your keys (add keys)

---
+++ change_keys.py
@@ -0,0 +1,11 @@
+#!flask/bin/python
+"""
+This will add another encryption key.  Any new notes or resaved notes will use the new key.
+The old key is not deleted as it is used to decrypt the messages already encrypted using that key.
+You can do this as many times as you want.  It is all automatic.
+"""
+import keyczar
+import os
+from keyczar import keyczart
+newpath = r'tmp/kz' 
+keyczart.main(['addkey','--location=tmp/kz/' ,'--status=primary'])
