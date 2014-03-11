2cd619ae09881a98471a0a4fded652870b548a45
Derek Lowes <derek3x@vmail.me>
2014-03-10 21:05:43 -0400

    added terms of use, privacy, and license info on files. -legal day

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
+"""
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2014 Derek Lowes (derek3x)
+"""
+
 from flask import render_template, flash, redirect, session, url_for, request, g, request, send_from_directory
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
@@ -791,3 +808,12 @@ def filecabinet_check_out(fc):
         flash('That File Cabinet ID does not belong to your account.', 'danger')
         return False
     return True
+
+#=====================Terms and Privacy=====================#
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
